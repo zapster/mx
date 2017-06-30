8b64907ff0db80bbc4443cd5215a62ce546d2b3a
Josef Eisl <josef.eisl@jku.at>
2017-06-30 17:55:59 +0200

    Bump mx version

--- mx.py
+++ mx.py
@@ -15851,7 +15851,7 @@ def main():
         abort(1, killsig=signal.SIGINT)
 
 # The comment after VersionSpec should be changed in a random manner for every bump to force merge conflicts!
-version = VersionSpec("5.114.10")  # Tasty tests
+version = VersionSpec("5.115.0")  # Make JMH flexible again!
 
 currentUmask = None
 _mx_start_datetime = datetime.utcnow()
